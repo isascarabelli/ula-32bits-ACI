80cce643fc983097bf02aa7304e3e35e1ec74843
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2022-05-16 12:02:50 -0300

    Update mult3x1.vhd

--- mult3x1.vhd
+++ mult3x1.vhd
@@ -5,8 +5,9 @@ use ieee.std_logic_1164.all;
 entity mult3x1 is
 
   port (e1, e2, e3: in std_logic;
-  		sel: in std_logic_vector (1 downto 0);
-		s: out std_logic);
+  	sel1: in std_logic;
+        sel2: in std_logic;
+	s: out std_logic);
 
 end mult3x1;
 
@@ -14,11 +15,9 @@ architecture arch_mult3x1 of mult3x1 is
 
 begin
 
-  with sel select
-
-  s <= e1 when "00",
-       e2 when "01",
-       e3 when others;
+  s <= e1 when (sel1 = '0' and sel2 = '0') else
+       e2 when (sel1 = '0' and sel2 = '1') else
+       e3;
 
 end arch_mult3x1;
   
